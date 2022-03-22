b585bdb31fe3d30618b8653cca71461c09ca5bff
Marcello Tridenti <marcello.tridenti@tutanota.com>
2022-03-22 11:43:35 +0100

    Fixed paths and interaction layers

--- Demo/Editor/DemoSceneLoader.cs
+++ Demo/Editor/DemoSceneLoader.cs
@@ -9,7 +9,7 @@ namespace VRBuilder.Editor.DemoScene
     /// </summary>
     public static class DemoSceneLoader
     {
-        private const string demoScenePath = "Assets/MindPort/VR Builder/Add-ons/Downloader/Source/DemoScene/Runtime/Scenes/VR Builder Demo - Core.unity";
+        private const string demoScenePath = "Assets/MindPort/VR Builder/Core/Demo/Runtime/Scenes/VR Builder Demo - Core.unity";
         private const string demoProcessFilePath = "Assets/StreamingAssets/Processes/Demo - Core/Demo - Core.json";
 
         [MenuItem("Tools/VR Builder/Demo Scenes/Core", false, 64)]
@@ -20,7 +20,7 @@ namespace VRBuilder.Editor.DemoScene
                 if(EditorUtility.DisplayDialog("Demo Scene Setup", "Before opening the demo scene, the sample process needs to be copied in Assets/StreamingAssets. Press Ok to proceed.", "Ok"))
                 {
                     Directory.CreateDirectory("Assets/StreamingAssets/Processes/Demo - Core");
-                    FileUtil.CopyFileOrDirectory("Assets/MindPort/VR Builder/Add-ons/Downloader/Source/StreamingAssets/Processes/Demo - Core/Demo - Core.json", demoProcessFilePath);
+                    FileUtil.CopyFileOrDirectory("Assets/MindPort/VR Builder/Core/Demo/StreamingAssets/Processes/Demo - Core/Demo - Core.json", demoProcessFilePath);
                 }
             }
 
