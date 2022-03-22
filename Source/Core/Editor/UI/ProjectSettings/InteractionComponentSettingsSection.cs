d3b873ca6a1c72a277319b80630fcdfa0371f0a9
Marcello Tridenti <marcello.tridenti@tutanota.com>
2022-03-22 17:51:29 +0100

    Polished settings page

--- Source/Core/Editor/UI/ProjectSettings/InteractionComponentSettingsSection.cs
+++ Source/Core/Editor/UI/ProjectSettings/InteractionComponentSettingsSection.cs
@@ -7,7 +7,7 @@ namespace VRBuilder.Editor.UI
 {
     internal class InteractionComponentSettingsSection : IProjectSettingsSection
     {
-        public string Title { get; } = "Interaction Component Configuration";
+        public string Title { get; } = "Interaction Component";
         public Type TargetPageProvider { get; } = typeof(BuilderSettingsProvider);
         public int Priority { get; } = 500;
 
@@ -15,9 +15,13 @@ namespace VRBuilder.Editor.UI
         {
             InteractionComponentSettings config = InteractionComponentSettings.Instance;
 
+            GUILayout.Label("You might want to disable the built-in interaction component in order to use a custom one or a partner integration.\n" +
+                "More interaction components, such as integrations with our partners, are avaliable on our Add-ons and Integrations page.", BuilderEditorStyles.Paragraph);
+            BuilderGUILayout.DrawLink("Add-ons and Integrations", "https://www.mindport.co/vr-builder-add-ons-and-integrations", BuilderEditorStyles.Indent);
+
             EditorGUI.BeginChangeCheck();
 
-            config.EnableXRInteractionComponent = GUILayout.Toggle(config.EnableXRInteractionComponent, "Enable XR Interaction Component", BuilderEditorStyles.Toggle);
+            config.EnableXRInteractionComponent = GUILayout.Toggle(config.EnableXRInteractionComponent, "Enable built-in XR Interaction Component", BuilderEditorStyles.Toggle);
 
             if (EditorGUI.EndChangeCheck())
             {
