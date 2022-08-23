9236c2f0b80facd4cd90982f2f1fba35cac6141d
Marcello Tridenti <marcello.tridenti@tutanota.com>
2022-08-23 17:37:43 +0200

    New animations

--- Source/XR-Interaction-Component/Source/Runtime/Hands/HandAnimatorController.cs
+++ Source/XR-Interaction-Component/Source/Runtime/Hands/HandAnimatorController.cs
@@ -1,54 +1,53 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-using VRBuilder.XRInteraction;
 
-[RequireComponent(typeof(Animator))]
-public class HandAnimatorController : MonoBehaviour
+namespace VRBuilder.XRInteraction.Animation
 {
-    private const string grabParameter = "Grab";
-    private const string pointParameter = "Point";
-
-    private Animator animator;
-    private ActionBasedController baseController;
-    private ActionBasedController teleportController;
-    private ActionBasedController uiController;
-    private ActionBasedControllerManager controllerManager;
-    private bool isInitialized = false;
-
-    private void Start()
+    [RequireComponent(typeof(Animator))]
+    public class HandAnimatorController : MonoBehaviour
     {
-        animator = GetComponent<Animator>();
-        controllerManager = GetComponentInParent<ActionBasedControllerManager>();
-        baseController = controllerManager.BaseController.GetComponent<ActionBasedController>();
-        teleportController = controllerManager.TeleportController.GetComponent<ActionBasedController>();
-        uiController = controllerManager.UIController.GetComponent<ActionBasedController>();
-        if(baseController != null && teleportController != null && uiController != null)
-        {
-            isInitialized = true;
-        }
-    }
+        private const string grabParameter = "Hand";
+        private const string useParameter = "Index";
+        private const string pointParameter = "Point";
 
-    private void Update()
-    {
-        if(isInitialized == false)
+        private Animator animator;
+        private ActionBasedController baseController;
+        private ActionBasedController teleportController;
+        private ActionBasedController uiController;
+        private ActionBasedControllerManager controllerManager;
+        private bool isInitialized = false;
+
+        private void Start()
         {
-            return;
+            animator = GetComponent<Animator>();
+            controllerManager = GetComponentInParent<ActionBasedControllerManager>();
+            baseController = controllerManager.BaseController.GetComponent<ActionBasedController>();
+            teleportController = controllerManager.TeleportController.GetComponent<ActionBasedController>();
+            uiController = controllerManager.UIController.GetComponent<ActionBasedController>();
+            if (baseController != null && teleportController != null && uiController != null)
+            {
+                isInitialized = true;
+            }
         }
-        
-        if(controllerManager.SelectState.Enabled)
+
+        private void Update()
         {
+            if (isInitialized == false)
+            {
+                return;
+            }
+
+            if(controllerManager.UIState.Enabled)
+            {
+                animator.SetBool(pointParameter, true);
+            }
+            else
+            {
+                animator.SetBool(pointParameter, false);
+            }
+
             animator.SetFloat(grabParameter, baseController.selectActionValue.action.ReadValue<float>());
-            animator.SetFloat(pointParameter, baseController.activateActionValue.action.ReadValue<float>());
-        }
-        else if(controllerManager.TeleportState.Enabled)
-        {
-            animator.SetFloat(grabParameter, baseController.selectActionValue.action.ReadValue<float>());
-            animator.SetFloat(pointParameter, baseController.activateActionValue.action.ReadValue<float>());
-        }
-        else if (controllerManager.UIState.Enabled)
-        {
-            animator.SetFloat(grabParameter, Mathf.Clamp01(animator.GetFloat(grabParameter) - Time.deltaTime * 10));
-            animator.SetFloat(pointParameter, baseController.selectActionValue.action.ReadValue<float>());
+            animator.SetFloat(useParameter, baseController.activateActionValue.action.ReadValue<float>());
         }
     }
 }
