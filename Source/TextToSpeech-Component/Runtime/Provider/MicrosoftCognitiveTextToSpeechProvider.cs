f8d893523e2a763e25f0bd3b31b85937ab4db85f
Aron Schaub <a.schaub@lefx.de>
2022-10-19 12:45:54 +0200

    chg: made microsoft cognitive service optional extra package.

--- Source/TextToSpeech-Component/Runtime/Provider/MicrosoftCognitiveTextToSpeechProvider.cs
+++ Source/TextToSpeech-Component/Runtime/Provider/MicrosoftCognitiveTextToSpeechProvider.cs
@@ -1,4 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿/******
+/// this is a optional plugin and needs the Microsoft cognitive speech installed, which you can download here: https://aka.ms/csspeech/unitypackage
+/// Due to limitations in assembly definition you have to set the MICROSOFT_COGNITIVE_SERVICES constraint manually in player settings after installation. 
+****/ 
+#if MICROSOFT_COGNITIVE_SERVICES
+using System.Threading.Tasks;
 using Microsoft.CognitiveServices.Speech;
 using UnityEngine;
 
@@ -24,3 +29,4 @@ namespace VRBuilder.TextToSpeech
         }
     }
 }
+#endif
