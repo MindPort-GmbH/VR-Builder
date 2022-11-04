0017e95ac49b18622aeecc1b036f42defa5bb43c
Marcello Tridenti <marcello.tridenti@tutanota.com>
2022-11-04 12:08:32 +0100

    Basic warning for missing properties

--- Source/Core/Editor/UI/Drawers/SceneObjectTagDrawer.cs
+++ Source/Core/Editor/UI/Drawers/SceneObjectTagDrawer.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using VRBuilder.Core.Configuration;
+using VRBuilder.Core.Properties;
 using VRBuilder.Core.SceneObjects;
 using VRBuilder.Core.Settings;
+using VRBuilder.Editor.UndoRedo;
 
 namespace VRBuilder.Editor.UI.Drawers
 {
@@ -12,20 +15,28 @@ namespace VRBuilder.Editor.UI.Drawers
     public class SceneObjectTagDrawer : AbstractDrawer
     {
         private const string noComponentSelected = "<none>";
+        protected bool isUndoOperation;
 
         public override Rect Draw(Rect rect, object currentValue, Action<object> changeValueCallback, GUIContent label)
         {
-            rect.height = EditorDrawingHelper.SingleLineHeight;
 
             SceneObjectTagBase sceneObjectTag = (SceneObjectTagBase)currentValue;
             Guid oldGuid = sceneObjectTag.Guid;
 
             SceneObjectTags.Tag[] tags = SceneObjectTags.Instance.Tags.ToArray();
             List<string> labels = tags.Select(tag => tag.Label).ToList();
-
-            EditorGUI.BeginDisabledGroup(tags.Length == 0);                        
-
             SceneObjectTags.Tag currentTag = tags.FirstOrDefault(tag => tag.Guid == oldGuid);
+            Rect guiLineRect = rect;
+
+            if (currentTag != null)
+            {
+                foreach (ISceneObject sceneObject in RuntimeConfigurator.Configuration.SceneObjectRegistry.GetByTag(currentTag.Guid))
+                {
+                    CheckForMisconfigurationIssues(sceneObject.GameObject, sceneObjectTag.GetReferenceType(), ref rect, ref guiLineRect);
+                }
+            }
+
+            EditorGUI.BeginDisabledGroup(tags.Length == 0);
 
             int selectedTagIndex = Array.IndexOf(tags, currentTag);
             bool isTagInvalid = false;
@@ -37,7 +48,7 @@ namespace VRBuilder.Editor.UI.Drawers
                 isTagInvalid = true;
             }
 
-            selectedTagIndex = EditorGUI.Popup(rect, label.text, selectedTagIndex, labels.ToArray());
+            selectedTagIndex = EditorGUI.Popup(guiLineRect, label.text, selectedTagIndex, labels.ToArray());
             EditorGUI.EndDisabledGroup();
 
             if(isTagInvalid && selectedTagIndex == 0)
@@ -68,6 +79,76 @@ namespace VRBuilder.Editor.UI.Drawers
             }
 
             return rect;
+        }
+
+        protected void CheckForMisconfigurationIssues(GameObject selectedSceneObject, Type valueType, ref Rect originalRect, ref Rect guiLineRect)
+        {
+            if (selectedSceneObject != null && selectedSceneObject.GetComponent(valueType) == null)
+            {
+                string warning = $"{selectedSceneObject.name} is not configured as {valueType.Name}";
+                const string button = "Fix it";
+                EditorGUI.HelpBox(guiLineRect, warning, MessageType.Warning);
+                guiLineRect = AddNewRectLine(ref originalRect);
+
+                if (GUI.Button(guiLineRect, button))
+                {
+                    // Only relevant for Undoing a Process Property.
+                    bool isAlreadySceneObject = selectedSceneObject.GetComponent<ProcessSceneObject>() != null && typeof(ISceneObjectProperty).IsAssignableFrom(valueType);
+                    Component[] alreadyAttachedProperties = selectedSceneObject.GetComponents(typeof(Component));
+
+                    RevertableChangesHandler.Do(
+                        new ProcessCommand(
+                            () => SceneObjectAutomaticSetup(selectedSceneObject, valueType),
+                            () => UndoSceneObjectAutomaticSetup(selectedSceneObject, valueType, isAlreadySceneObject, alreadyAttachedProperties)));
+                }
+
+                guiLineRect = AddNewRectLine(ref originalRect);
+            }
+        }
+
+        protected Rect AddNewRectLine(ref Rect currentRect)
+        {
+            Rect newRectLine = currentRect;
+            newRectLine.height = EditorDrawingHelper.SingleLineHeight;
+            newRectLine.y += currentRect.height + EditorDrawingHelper.VerticalSpacing;
+
+            currentRect.height += EditorDrawingHelper.SingleLineHeight + EditorDrawingHelper.VerticalSpacing;
+            return newRectLine;
+        }
+
+        protected void SceneObjectAutomaticSetup(GameObject selectedSceneObject, Type valueType)
+        {
+            ISceneObject sceneObject = selectedSceneObject.GetComponent<ProcessSceneObject>() ?? selectedSceneObject.AddComponent<ProcessSceneObject>();
+
+            if (RuntimeConfigurator.Configuration.SceneObjectRegistry.ContainsGuid(sceneObject.Guid) == false)
+            {
+                // Sets a UniqueName and then registers it.
+                sceneObject.SetSuitableName();
+            }
+
+            if (typeof(ISceneObjectProperty).IsAssignableFrom(valueType))
+            {
+                sceneObject.AddProcessProperty(valueType);
+            }
+
+            isUndoOperation = true;
+        }
+
+        private void UndoSceneObjectAutomaticSetup(GameObject selectedSceneObject, Type valueType, bool hadProcessComponent, Component[] alreadyAttachedProperties)
+        {
+            ISceneObject sceneObject = selectedSceneObject.GetComponent<ProcessSceneObject>();
+
+            if (typeof(ISceneObjectProperty).IsAssignableFrom(valueType))
+            {
+                sceneObject.RemoveProcessProperty(valueType, true, alreadyAttachedProperties);
+            }
+
+            if (hadProcessComponent == false)
+            {
+                UnityEngine.Object.DestroyImmediate((ProcessSceneObject)sceneObject);
+            }
+
+            isUndoOperation = true;
         }
     }
 }
