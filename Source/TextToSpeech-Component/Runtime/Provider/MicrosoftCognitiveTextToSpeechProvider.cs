e313d8671c62846613ad2dac7de33536d674591e
Marcello Tridenti <marcello.tridenti@tutanota.com>
2022-11-10 10:35:16 +0100

    Revert "chg: made microsoft cognitive service optional extra package."
    This reverts commit f8d893523e2a763e25f0bd3b31b85937ab4db85f.

--- Source/TextToSpeech-Component/Runtime/Provider/MicrosoftCognitiveTextToSpeechProvider.cs
+++ Source/TextToSpeech-Component/Runtime/Provider/MicrosoftCognitiveTextToSpeechProvider.cs
@@ -1,9 +1,4 @@
-﻿/******
-/// this is a optional plugin and needs the Microsoft cognitive speech installed, which you can download here: https://aka.ms/csspeech/unitypackage
-/// Due to limitations in assembly definition you have to set the MICROSOFT_COGNITIVE_SERVICES constraint manually in player settings after installation. 
-****/ 
-#if MICROSOFT_COGNITIVE_SERVICES
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.CognitiveServices.Speech;
 using UnityEngine;
 
@@ -29,4 +24,3 @@ namespace VRBuilder.TextToSpeech
         }
     }
 }
-#endif
