04de55ddbc2fc7806a8b57a71b06f4e579d8368a
Marcello Tridenti <marcello.tridenti@tutanota.com>
2022-11-29 16:57:54 +0100

    Drawer locator is initialized on load

--- Source/Core/Editor/UI/Drawers/DrawerLocator.cs
+++ Source/Core/Editor/UI/Drawers/DrawerLocator.cs
@@ -6,11 +6,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using UnityEditor;
 using VRBuilder.Core.Attributes;
 using VRBuilder.Core.Utils;
 
 namespace VRBuilder.Editor.UI.Drawers
 {
+    [InitializeOnLoad]
     public static class DrawerLocator
     {
         private static readonly Dictionary<Type, IProcessDrawer> allDrawers;
