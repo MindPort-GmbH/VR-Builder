3ab91ac37f2e6d36df7a1277ab58e1b41d8fade8
Marcello Tridenti <marcello.tridenti@tutanota.com>
2022-12-07 14:49:03 +0100

    Expand and explode

--- Source/Core/Editor/UI/GraphView/StepGroupNode.cs
+++ Source/Core/Editor/UI/GraphView/StepGroupNode.cs
@@ -1,11 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
 using VRBuilder.Core;
+using VRBuilder.Core.Behaviors;
 
 namespace VRBuilder.Editor.UI.Graphics
 {
     public class StepGroupNode : StepGraphNode
     {
+        private ExecuteChapterBehavior behavior;
+        protected ExecuteChapterBehavior Behavior
+        {
+            get
+            {
+                if (behavior == null)
+                {
+                    behavior = (ExecuteChapterBehavior)step.Data.Behaviors.Data.Behaviors.FirstOrDefault(behavior => behavior is ExecuteChapterBehavior);
+                }
+
+                return behavior;
+            }
+        }
+
         public StepGroupNode(IStep step) : base(step)
         {
+            titleButtonContainer.Clear();
+            extensionContainer.style.backgroundColor = new Color(.2f, .2f, .2f, .8f);
+
+            DrawExpandButton();
+        }
+
+        private void DrawExpandButton()
+        {
+            Button expandButton = new Button(() => OnClickExpand());
+            Label expandLabel = new Label("Expand");
+            expandButton.contentContainer.Add(expandLabel);
+            extensionContainer.Add(expandButton);
+
+            Button explodeButton = new Button(() => OnClickExplode());
+            Label explodeLabel = new Label("Explode");
+            explodeButton.contentContainer.Add(explodeLabel);
+            extensionContainer.Add(explodeButton);
+        }
+
+        private void OnClickExplode()
+        {
+            IChapter currentChapter = GlobalEditorHandler.GetCurrentChapter();
+            IEnumerable<ITransition> leadingTransitions = currentChapter.Data.Steps.SelectMany(step => step.Data.Transitions.Data.Transitions).Where(transition => transition.Data.TargetStep == step);
+
+            foreach(IStep addedStep in Behavior.Data.Chapter.Data.Steps)
+            {
+                currentChapter.Data.Steps.Add(addedStep);
+                addedStep.StepMetadata.Position = step.StepMetadata.Position;
+            }
+
+            foreach(ITransition transition in leadingTransitions)
+            {
+                transition.Data.TargetStep = Behavior.Data.Chapter.Data.FirstStep;
+            }
+
+            if(currentChapter.Data.FirstStep == step)
+            {
+                currentChapter.Data.FirstStep = Behavior.Data.Chapter.Data.FirstStep;
+            }
+
+            currentChapter.Data.Steps.Remove(step);
+
+            GlobalEditorHandler.RequestNewChapter(currentChapter);
+        }
+
+        private void OnClickExpand()
+        {
+            GlobalEditorHandler.RequestNewChapter(Behavior.Data.Chapter);
+        }
+
+        public override void OnSelected()
+        {
+            base.OnSelected();
+
+            GlobalEditorHandler.ChangeCurrentStep(null);
         }
     }
 }
--- Source/Core/Editor/UI/GraphView/StepGroupPostProcessing.cs
+++ Source/Core/Editor/UI/GraphView/StepGroupPostProcessing.cs
@@ -12,27 +12,6 @@ namespace VRBuilder.Core
                 entity.Data.Behaviors.Data.Behaviors.Add(new ExecuteChapterBehavior(group));
 
                 entity.Data.Transitions.Data.Transitions.Add(EntityFactory.CreateTransition());
-
-                //Debug.Log("Postprocessing group");
-
-                //StepGroup stepGroup = (StepGroup)entity;
-
-                IStep step1 = EntityFactory.CreateStep("step 1");
-                step1.Data.Behaviors.Data.Behaviors.Add(new DelayBehavior(2));
-
-                IStep step2 = EntityFactory.CreateStep("step 2");
-                step2.Data.Behaviors.Data.Behaviors.Add(new DelayBehavior(2));
-
-                step1.Data.Transitions.Data.Transitions[0].Data.TargetStep = step2;
-
-                group.Data.Steps.Add(step1);
-                group.Data.Steps.Add(step2);
-                group.Data.FirstStep = step1;
-
-                //stepGroup.Data.Steps.Add(step1);
-                //stepGroup.Data.Steps.Add(step2);
-
-                //stepGroup.Data.FirstStep = step1;
             }
         }
     }
