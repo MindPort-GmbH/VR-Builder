bbc66eb4fcfc97b4314601f957677c5d361e267a
VaLiuM09 <marcello.tridenti@tutanota.com>
2023-02-03 11:34:54 +0100

    Moved to separate assembly

--- Source/Networking-Component/Runtime/PlayerCameraEnabler.cs
+++ Source/Networking-Component/Runtime/PlayerCameraEnabler.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.InputSystem.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
+using VRBuilder.XRInteraction;
 
 namespace VRBuilder.Networking
 {
@@ -16,18 +17,29 @@ namespace VRBuilder.Networking
 
             if (IsOwner == false)
             {
+                foreach (ActionBasedControllerManager manager in GetComponentsInChildren<ActionBasedControllerManager>())
+                {
+                    Destroy(manager);
+                }
+
                 Debug.Log($"Disabling input for rig {OwnerClientId}");
                 inputActionManager.DisableInput();
 
-                foreach(XRBaseController controller in GetComponentsInChildren<XRBaseController>())
+                foreach (XRBaseController controller in GetComponentsInChildren<XRBaseController>())
                 {
-                    Destroy(controller);
+                    controller.enableInputActions = false;
+                    controller.enableInputTracking= false;
                 }
 
-                foreach(TrackedPoseDriver driver in GetComponentsInChildren<TrackedPoseDriver>())
+                foreach (TrackedPoseDriver driver in GetComponentsInChildren<TrackedPoseDriver>())
                 {
                     Destroy(driver);
                 }
+
+                //foreach(XRBaseInteractor interactor in GetComponentsInChildren<XRBaseInteractor>())
+                //{
+                //    Destroy(interactor);
+                //}
 
                 foreach (Camera camera in GetComponentsInChildren<Camera>())
                 {
@@ -40,6 +52,10 @@ namespace VRBuilder.Networking
                         meshRenderer.enabled = true;
                     }
                 }
+            }
+            else
+            {
+                Debug.Log($"Rig {OwnerClientId} is player rig");
             }
         }
     }
