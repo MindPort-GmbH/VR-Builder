7dc0fc447b59dc4c917e82f9ee77bf4bf8cc1e2d
Marcello Tridenti <marcello.tridenti@tutanota.com>
2023-03-03 11:12:00 +0100

    Back to one page

--- Source/Core/Editor/Setup/RigLoaderSceneSetupConfiguration.cs
+++ Source/Core/Editor/Setup/RigLoaderSceneSetupConfiguration.cs
@@ -11,8 +11,8 @@ namespace VRBuilder.Editor.Setup
 
         public string DefaultProcessController => typeof(StandardProcessController).AssemblyQualifiedName;
 
-        public string Description => "Similar to the default configuration, except there is no rig in the editor scene. The rig is spawned at runtime by" +
-            "the INTERACTION_RIG_LOADER object at the DUMMY_USER position. This can be useful for advanced use cases requiring to switch rig at runtime," +
+        public string Description => "Similar to the default configuration, except there is no rig in the editor scene. The rig is spawned at runtime by " +
+            "the INTERACTION_RIG_LOADER object at the DUMMY_USER position. This can be useful for advanced use cases requiring to switch rig at runtime, " +
             "but it makes it harder to customize the rig.";
 
         public IEnumerable<string> GetSetupNames()
--- Source/Core/Editor/UI/Wizard/Setup/ProcessSceneSetupPage.cs
+++ Source/Core/Editor/UI/Wizard/Setup/ProcessSceneSetupPage.cs
@@ -16,13 +16,42 @@ namespace VRBuilder.Editor.UI.Wizard
     /// </summary>
     internal class ProcessSceneSetupPage : WizardPage
     {
+        private const int MaxProcessNameLength = 40;
+        private const int MinHeightOfInfoText = 30;
+
+        public static ISceneSetupConfiguration Configuration { get; set; }
+
+        [SerializeField]
+        private bool useCurrentScene = true;
+
+        [SerializeField]
+        private bool createNewScene = false;
+
+        [SerializeField]
+        private bool createNewProcess = true;
+
+        [SerializeField]
+        private bool setupScene = true;
+
+        [SerializeField]
+        private string processName = "My VR Process";
+
+        [SerializeField]
+        private string lastCreatedProcess = null;
+
         [SerializeField]
         private int selectedIndex = 0;
 
         private readonly ISceneSetupConfiguration[] configurations;
 
+        private readonly GUIContent infoContent;
+        private readonly GUIContent warningContent;
+
         public ProcessSceneSetupPage() : base("Setup Process")
         {
+            infoContent = EditorGUIUtility.IconContent("console.infoicon.inactive.sml");
+            warningContent = EditorGUIUtility.IconContent("console.warnicon.sml");
+
             configurations = ReflectionUtils.GetConcreteImplementationsOf<ISceneSetupConfiguration>()
                 .Select(type => ReflectionUtils.CreateInstanceOfType(type))
                 .Cast<ISceneSetupConfiguration>()
@@ -34,13 +63,96 @@ namespace VRBuilder.Editor.UI.Wizard
         {
             GUILayout.BeginArea(window);
 
-            GUILayout.Label("Setup VR Builder scene", BuilderEditorStyles.Title);
+            GUILayout.Label("Setup Process", BuilderEditorStyles.Title);
 
-            GUILayout.Label("Select a scene configuration", BuilderEditorStyles.Header);
+            createNewProcess = GUILayout.Toggle(createNewProcess, "Create a new process", BuilderEditorStyles.Toggle);
+            setupScene = GUILayout.Toggle(setupScene, "Setup the scene for VR Builder", BuilderEditorStyles.Toggle);
 
-            selectedIndex = EditorGUILayout.Popup(selectedIndex, configurations.Select(config => config.Name).ToArray());
+            if(createNewProcess && !setupScene)
+            {
+                EditorGUILayout.HelpBox("The new process will not work unless the scene is set up for VR Builder. Proceed only if you mean to add a new process " +
+                    "to an already configured scene.", MessageType.Warning);
+            }
 
-            EditorGUILayout.HelpBox(configurations[selectedIndex].Description, MessageType.Info);
+            GUILayout.Space(16);
+
+            if (createNewProcess)
+            {
+                GUILayout.Label("Name of your VR Process", BuilderEditorStyles.Header);
+                processName = BuilderGUILayout.DrawTextField(processName, MaxProcessNameLength, GUILayout.Width(window.width * 0.7f));
+                GUI.enabled = true;
+
+                if (ProcessAssetUtils.CanCreate(processName, out string errorMessage) == false && lastCreatedProcess != processName)
+                {
+                    GUIContent processWarningContent = warningContent;
+                    processWarningContent.text = errorMessage;
+
+                    GUILayout.Label(processWarningContent, BuilderEditorStyles.Label, GUILayout.MinHeight(MinHeightOfInfoText));
+
+                    CanProceed = false;
+                }
+                else
+                {
+                    GUILayout.Space(MinHeightOfInfoText + BuilderEditorStyles.BaseIndent);
+                    CanProceed = true;
+                }
+
+                GUILayout.BeginHorizontal();
+                GUILayout.Space(BuilderEditorStyles.Indent);
+                GUILayout.BeginVertical();
+                bool isUseCurrentScene = GUILayout.Toggle(useCurrentScene, "Take my current scene", BuilderEditorStyles.RadioButton);
+                if (useCurrentScene == false && isUseCurrentScene)
+                {
+                    useCurrentScene = true;
+                    createNewScene = false;
+                }
+
+                bool isCreateNewScene = GUILayout.Toggle(createNewScene, "Create a new scene", BuilderEditorStyles.RadioButton);
+                if (createNewScene == false && isCreateNewScene)
+                {
+                    createNewScene = true;
+                    useCurrentScene = false;
+                }
+                GUILayout.EndVertical();
+                GUILayout.EndHorizontal();
+
+                if (createNewScene)
+                {
+                    GUIContent helpContent;
+                    string sceneInfoText = "Scene will have the same name as the process.";
+                    if (SceneSetupUtils.SceneExists(processName))
+                    {
+                        sceneInfoText += " Scene already exists";
+                        CanProceed = false;
+                        helpContent = warningContent;
+                    }
+                    else
+                    {
+                        helpContent = infoContent;
+                    }
+
+                    helpContent.text = sceneInfoText;
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Space(BuilderEditorStyles.Indent);
+                        EditorGUILayout.LabelField(helpContent, BuilderEditorStyles.Label, GUILayout.MinHeight(MinHeightOfInfoText));
+                    }
+                    GUILayout.EndHorizontal();
+                }
+
+                GUILayout.Space(16);
+            }
+
+            CanProceed |= createNewProcess == false;
+
+            if (setupScene)
+            {
+                GUILayout.Label("Select a scene configuration", BuilderEditorStyles.Header);
+
+                selectedIndex = EditorGUILayout.Popup(selectedIndex, configurations.Select(config => config.Name).ToArray(), GUILayout.Width(200));
+
+                EditorGUILayout.HelpBox(configurations[selectedIndex].Description, MessageType.Info);
+            }
 
             GUILayout.EndArea();
         }
@@ -48,7 +160,27 @@ namespace VRBuilder.Editor.UI.Wizard
         /// <inheritdoc />
         public override void Apply()
         {
-            ProcessCreationPage.Configuration = configurations[selectedIndex];
+            if (processName == lastCreatedProcess)
+            {
+                return;
+            }
+
+            if (createNewProcess && useCurrentScene == false)
+            {
+                SceneSetupUtils.CreateNewScene(processName);
+            }
+
+            if(setupScene)
+            {
+                ProcessSceneSetup.Run(configurations[selectedIndex]);
+            }
+
+            if (createNewProcess)
+            {
+                SceneSetupUtils.SetupProcess(processName);
+            }
+
+            lastCreatedProcess = processName;
             EditorWindow.FocusWindowIfItsOpen<WizardWindow>();
         }
     }
