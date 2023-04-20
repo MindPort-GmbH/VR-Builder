84a145960a2915505978b1d5f9cd1f76c081ab17
VaLiuM09 <marcello.tridenti@tutanota.com>
2023-04-20 10:12:05 +0200

    chore: Updated copyright notice in file headers

--- Source/Core/Editor/UI/Tabs/DynamicTab.cs
+++ Source/Core/Editor/UI/Tabs/DynamicTab.cs
@@ -1,6 +1,6 @@
 // Copyright (c) 2013-2019 Innoactive GmbH
 // Licensed under the Apache License, Version 2.0
-// Modifications copyright (c) 2021-2022 MindPort GmbH
+// Modifications copyright (c) 2021-2023 MindPort GmbH
 
 using System;
 using UnityEngine;
