5e5e41b83531f424a8361784241f4271cc4527cd
VaLiuM09 <marcello.tridenti@tutanota.com>
2023-04-24 13:53:54 +0200

    Now also finds disabled objects

--- Source/Core/Editor/UI/Menu/AddPropertyExtensionsMenuEntry.cs
+++ Source/Core/Editor/UI/Menu/AddPropertyExtensionsMenuEntry.cs
@@ -12,14 +12,14 @@ namespace VRBuilder.Editor.BuilderMenu
     /// </summary>
     internal static class AddPropertyExtensionsMenuEntry
     {
-        [MenuItem("Tools/VR Builder/Developer/Add Scene Property Extensions", false, 81)]
+        [MenuItem("Tools/VR Builder/Developer/Add Scene Property Extensions", false, 70)]
         private static void AddPropertyExtensions()
         {
             if(EditorUtility.DisplayDialog("Add Scene Property Extensions?", "This will add the extensions required by the current scene setup to all scene object properties in the scene.\n" +
                 "Previously added extensions will not be removed.\n" +
                 "Continue?", "Ok", "Cancel"))
             {
-                IEnumerable<ProcessSceneObject> processSceneObjects = GameObject.FindObjectsOfType<ProcessSceneObject>();
+                IEnumerable<ProcessSceneObject> processSceneObjects = GameObject.FindObjectsOfType<ProcessSceneObject>(true);
                 float processedObjects = 0;
 
                 foreach (ProcessSceneObject processSceneObject in processSceneObjects)
