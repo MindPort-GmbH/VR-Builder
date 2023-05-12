f97938eeb8c938fdd73dabe848bdd6dcba85e4b2
VaLiuM09 <marcello.tridenti@tutanota.com>
2023-05-12 09:34:40 +0200

    Added roadmap menu entry (#101)

--- Source/Core/Editor/UI/Menu/ReviewMenuEntry.cs
+++ Source/Core/Editor/UI/Menu/ReviewMenuEntry.cs
@@ -9,7 +9,7 @@ namespace VRBuilder.Editor.BuilderMenu
         /// Redirects to the VR Builder asset store review page.
         /// </summary>
         [MenuItem("Tools/VR Builder/Leave a Review", false, 128)]
-        private static void OpenCommunityPage()
+        private static void OpenReviewPage()
         {
             Application.OpenURL("https://assetstore.unity.com/packages/tools/visual-scripting/vr-builder-open-source-toolkit-for-vr-creation-201913#reviews");
         }
