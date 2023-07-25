94e0264fc51e6183bcd7ce4f91b857a4da2db32b
Markus Wellmann <mrwellmann@gmail.com>
2023-07-25 17:51:42 +0200

    Added pre build close Process Editor functionality

---
+++ Source/Core/Editor/Unity/PreBuildCloseProcessEditor.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using VRBuilder.Editor.UI.Windows;
+
+namespace VRBuilder.Editor
+{
+    /// <summary>
+    /// We are closing the process editor window before the build starts.
+    /// Having an open process editor window can lead to corrupt Unique Names.
+    /// </summary>
+    public class PreBuildCloseProcessEditor : IPreprocessBuildWithReport
+    {
+        ///<inheritdoc />
+        public int callbackOrder => 1;
+
+        ///<inheritdoc />
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            System.Type windowType = typeof(ProcessEditorWindow);
+
+            EditorWindow window = EditorWindow.GetWindow(windowType);
+
+            if (window != null)
+                window.Close();
+        }
+    }
+}
