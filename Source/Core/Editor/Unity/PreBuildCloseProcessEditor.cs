a2b67533bdb8c6df79d3004ee0b8911afeb18a71
Markus Wellmann <mrwellmann@users.noreply.github.com>
2023-07-26 17:42:03 +0200

    [MP-1815] Created script that runs on pre build and closes the Process Editor

---
+++ Source/Core/Editor/Unity/PreBuildCloseProcessEditor.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using VRBuilder.Editor.UI.Windows;
+
+namespace VRBuilder.Editor
+{
+    /// <summary>
+    /// We are closing the process editor window before the build starts.
+    /// Having an open process editor window can lead to corrupt Unique Names.
+    /// </summary>
+    public class PreBuildCloseProcessEditor : IPreprocessBuildWithReport
+    {
+        ///<inheritdoc />
+        public int callbackOrder => 1;
+
+        ///<inheritdoc />
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            System.Type windowType = typeof(ProcessEditorWindow);
+
+            if (EditorWindow.HasOpenInstances<ProcessEditorWindow>())
+            {
+                ProcessEditorWindow window = EditorWindow.GetWindow<ProcessEditorWindow>();
+                window.Close();
+
+                Debug.Log("Process Editor Window was closed before building as it can lead to corrupt Unique Names.");
+            }
+        }
+    }
+}
