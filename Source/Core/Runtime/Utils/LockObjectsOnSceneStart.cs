3af73ae58e58a53155b4181e6ca7254858b0ecb7
VaLiuM09 <marcello.tridenti@tutanota.com>
2023-08-10 17:17:17 +0200

    Revert "Scene objects are locked on scene start instead of properties"
    This reverts commit 9925c08aa820c60ae42c6cce2f719cadcaeeecc8.

--- Source/Core/Runtime/Utils/LockObjectsOnSceneStart.cs
+++ Source/Core/Runtime/Utils/LockObjectsOnSceneStart.cs
@@ -3,9 +3,9 @@
 // Modifications copyright (c) 2021-2023 MindPort GmbH
 
 using System.Linq;
+using VRBuilder.Core.Properties;
 using VRBuilder.Unity;
 using UnityEngine;
-using VRBuilder.Core.SceneObjects;
 
 namespace VRBuilder.Core.Utils
 {
@@ -21,7 +21,13 @@ namespace VRBuilder.Core.Utils
         // Start is called before the first frame update
         void Start()
         {
-            SceneUtils.GetActiveAndInactiveComponents<ProcessSceneObject>().ToList().ForEach(pso => pso.SetLocked(lockSceneObjectsOnSceneStart));
+            foreach(LockableProperty lockable in SceneUtils.GetActiveAndInactiveComponents<LockableProperty>())
+            {
+                if(lockable.LockOnParentObjectLock)
+                {
+                    lockable.SetLocked(lockSceneObjectsOnSceneStart);
+                }
+            }
         }
     }
 }
