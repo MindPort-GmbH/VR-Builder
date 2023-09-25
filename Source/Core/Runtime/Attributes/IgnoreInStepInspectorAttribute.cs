050dc3aa1ee5c59779e67ac9de8e06d3e2aa681c
Marcello Tridenti <marcello.tridenti@mindport.co>
2023-09-25 09:55:09 +0200

    Dynamic transition names in process editor (#140)
    * Added dynamic names for transitions in process editor
    
    * Added attribute to ignore a dynamic name in the step inspector

---
+++ Source/Core/Runtime/Attributes/IgnoreInStepInspectorAttribute.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace VRBuilder.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class IgnoreInStepInspectorAttribute : Attribute
+    {
+
+    }
+}
