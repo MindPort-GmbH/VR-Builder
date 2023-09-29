176adcf4f1926f63806c181991af09dab53b468f
Marcello Tridenti <marcello.tridenti@mindport.co>
2023-09-29 14:39:10 +0200

    Added github links in product (#143)
    * Added GitHub link to project setup wizard
    
    * Added GitHub links in documentation

--- Source/Core/Editor/UI/Wizard/Setup/AllAboutPage.cs
+++ Source/Core/Editor/UI/Wizard/Setup/AllAboutPage.cs
@@ -14,7 +14,7 @@ namespace VRBuilder.Editor.UI.Wizard
         [SerializeField]
         private bool loadDemoScene = true;
 
-        public AllAboutPage() : base("Help & Documentation", false ,false )
+        public AllAboutPage() : base("Help & Documentation", false, false)
         {
 
         }
@@ -22,35 +22,36 @@ namespace VRBuilder.Editor.UI.Wizard
         public override void Draw(Rect window)
         {
             GUILayout.BeginArea(window);
-                GUILayout.Label("Help & Documentation", BuilderEditorStyles.Title);
-                GUILayout.Label("Have a look at the following resources for further information.", BuilderEditorStyles.Paragraph);
-                GUILayout.Label("Help", BuilderEditorStyles.Header);
+            GUILayout.Label("Help & Documentation", BuilderEditorStyles.Title);
+            GUILayout.Label("Have a look at the following resources for further information.", BuilderEditorStyles.Paragraph);
+            GUILayout.Label("Help", BuilderEditorStyles.Header);
 
-                BuilderGUILayout.DrawLink("Documentation", "http://documentation.mindport.co/", BuilderEditorStyles.IndentLarge);
-                BuilderGUILayout.DrawLink("Tutorials", "https://www.mindport.co/vr-builder/tutorials", BuilderEditorStyles.IndentLarge);
-                BuilderGUILayout.DrawLink("Roadmap", "https://www.mindport.co/vr-builder/roadmap", BuilderEditorStyles.IndentLarge);
+            BuilderGUILayout.DrawLink("Documentation", "http://documentation.mindport.co/", BuilderEditorStyles.IndentLarge);
+            BuilderGUILayout.DrawLink("Tutorials", "https://www.mindport.co/vr-builder/tutorials", BuilderEditorStyles.IndentLarge);
+            BuilderGUILayout.DrawLink("Roadmap", "https://www.mindport.co/vr-builder/roadmap", BuilderEditorStyles.IndentLarge);
 
-                GUILayout.Label("Community", BuilderEditorStyles.Header);
+            GUILayout.Label("Community", BuilderEditorStyles.Header);
 
-                BuilderGUILayout.DrawLink("Community", "http://community.mindport.co", BuilderEditorStyles.IndentLarge);
-                BuilderGUILayout.DrawLink("Contact us", "mailto:info@mindport.co", BuilderEditorStyles.IndentLarge);
+            BuilderGUILayout.DrawLink("Community", "http://community.mindport.co", BuilderEditorStyles.IndentLarge);
+            BuilderGUILayout.DrawLink("Contact us", "mailto:info@mindport.co", BuilderEditorStyles.IndentLarge);
+            BuilderGUILayout.DrawLink("GitHub", "https://github.com/MindPort-GmbH", BuilderEditorStyles.IndentLarge);
 
-                GUILayout.Label("Review", BuilderEditorStyles.Header);
-                GUILayout.Label("If you like what we are doing, you can help us greatly by leaving a positive review on the Unity Asset Store!", BuilderEditorStyles.Paragraph);
+            GUILayout.Label("Review", BuilderEditorStyles.Header);
+            GUILayout.Label("If you like what we are doing, you can help us greatly by leaving a positive review on the Unity Asset Store!", BuilderEditorStyles.Paragraph);
 
-                BuilderGUILayout.DrawLink("Leave a review", "https://assetstore.unity.com/packages/tools/visual-scripting/vr-builder-open-source-toolkit-for-vr-creation-201913#reviews", BuilderEditorStyles.IndentLarge);
+            BuilderGUILayout.DrawLink("Leave a review", "https://assetstore.unity.com/packages/tools/visual-scripting/vr-builder-open-source-toolkit-for-vr-creation-201913#reviews", BuilderEditorStyles.IndentLarge);
 
-                GUILayout.Label("Demo Scene", BuilderEditorStyles.Title);     
+            GUILayout.Label("Demo Scene", BuilderEditorStyles.Title);
 
-                loadDemoScene = GUILayout.Toggle(loadDemoScene, "Load the demo scene after closing the wizard.", BuilderEditorStyles.Toggle);
+            loadDemoScene = GUILayout.Toggle(loadDemoScene, "Load the demo scene after closing the wizard.", BuilderEditorStyles.Toggle);
 
-                if (loadDemoScene)
-                {
-                    EditorGUILayout.HelpBox("VR Builder will automatically copy the process JSON to the StreamingAssets folder before opening the demo scene.", MessageType.Info);
-                }
+            if (loadDemoScene)
+            {
+                EditorGUILayout.HelpBox("VR Builder will automatically copy the process JSON to the StreamingAssets folder before opening the demo scene.", MessageType.Info);
+            }
 
-                GUILayout.Space(16);
-                GUILayout.Label("You can access the menu under Tools > VR Builder to load a demo scene at any time or create a new VR Builder scene.", BuilderEditorStyles.Paragraph);
+            GUILayout.Space(16);
+            GUILayout.Label("You can access the menu under Tools > VR Builder to load a demo scene at any time or create a new VR Builder scene.", BuilderEditorStyles.Paragraph);
             GUILayout.EndArea();
         }
 
