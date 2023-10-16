b34706aad971391af4d2fdfecae1166b6ac7644d
Markus Wellmann <mrwellmann@users.noreply.github.com>
2023-10-16 09:46:19 +0200

    Reverted path change

--- Demo/Editor/DemoSceneLoader.cs
+++ Demo/Editor/DemoSceneLoader.cs
@@ -9,7 +9,7 @@ namespace VRBuilder.Editor.DemoScene
     /// </summary>
     public static class DemoSceneLoader
     {
-        private const string demoScenePath = "Assets/MindPort/VR Builder/Core/VR-Builder/Demo/Runtime/Scenes/VR Builder Demo - Core Features.unity";
+        private const string demoScenePath = "Assets/MindPort/VR Builder/Core/Demo/Runtime/Scenes/VR Builder Demo - Core Features.unity";
         private const string demoProcessFilePath = "Assets/StreamingAssets/Processes/Demo - Core Features/Demo - Core Features.json";
 
         [MenuItem("Tools/VR Builder/Demo Scenes/Core", false, 64)]
@@ -25,7 +25,7 @@ namespace VRBuilder.Editor.DemoScene
             if (File.Exists(demoProcessFilePath) == false)
             {
                 Directory.CreateDirectory("Assets/StreamingAssets/Processes/Demo - Core Features");
-                FileUtil.CopyFileOrDirectory("Assets/MindPort/VR Builder/Core/VR-Builder/Demo/StreamingAssets/Processes/Demo - Core Features/Demo - Core Features.json", demoProcessFilePath);
+                FileUtil.CopyFileOrDirectory("Assets/MindPort/VR Builder/Core/Demo/StreamingAssets/Processes/Demo - Core Features/Demo - Core Features.json", demoProcessFilePath);
             }
 
             EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
