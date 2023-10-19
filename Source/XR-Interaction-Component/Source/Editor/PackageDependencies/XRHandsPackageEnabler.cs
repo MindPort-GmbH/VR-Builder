1b71d62ff10d3b110f81267d4b62bf7e4010dd89
VaLiuM09 <marcello.tridenti@tutanota.com>
2023-10-19 13:36:11 +0200

    Specified XR Hands version

--- Source/XR-Interaction-Component/Source/Editor/PackageDependencies/XRHandsPackageEnabler.cs
+++ Source/XR-Interaction-Component/Source/Editor/PackageDependencies/XRHandsPackageEnabler.cs
@@ -9,7 +9,7 @@ namespace VRBuilder.Editor.PackageManager.XRInteraction
         public override string Package { get; } = "com.unity.xr.hands";
 
         /// <inheritdoc/>
-        //public override string Version { get; set; } = "2.4.1";
+        public override string Version { get; set; } = "1.3.0";
 
         /// <inheritdoc/>
         public override string[] Samples { get; } = { "HandVisualizer" };
