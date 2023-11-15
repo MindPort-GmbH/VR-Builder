336a42a9237c822e18ef34532c0e3f6784bbdb44
Marcello Tridenti <marcello.tridenti@tutanota.com>
2023-11-15 12:38:52 +0100

    Added support for interaction layer configuration

--- Source/XR-Interaction-Component/Source/Runtime/User/InteractorLayerConfigurator.cs
+++ Source/XR-Interaction-Component/Source/Runtime/User/InteractorLayerConfigurator.cs
@@ -20,16 +20,17 @@ namespace VRBuilder.XRInteraction.User
         [Tooltip("Name of the raycast layer.")]
         private string raycastLayerName;
 
+        [SerializeField]
+        [Tooltip("Name of the interaction layer.")]
+        private string interactionLayerName;
+
         /// <inheritdoc/>
         public void ExecuteSetup()
         {
-            if (string.IsNullOrEmpty(raycastLayerName))
-            {
-                return;
-            }
-
             foreach (XRBaseInteractor interactor in interactors)
             {
+                SetupInteractionLayer(interactor);
+
                 if (interactor is XRRayInteractor rayInteractor)
                 {
                     SetupRaycastLayer(rayInteractor);
@@ -37,8 +38,36 @@ namespace VRBuilder.XRInteraction.User
             }
         }
 
+        private void SetupInteractionLayer(XRBaseInteractor interactor)
+        {
+            if (string.IsNullOrEmpty(interactionLayerName))
+            {
+                return;
+            }
+
+            InteractionLayerMask interactionLayer = InteractionLayerMask.NameToLayer(interactionLayerName);
+
+            if (interactionLayer.value < 0)
+            {
+                Debug.LogError($"Layer '{interactionLayerName}' does not exist.");
+                return;
+            }
+
+            if (interactor.interactionLayers != interactionLayer)
+            {
+                interactor.interactionLayers = interactionLayer;
+                EditorUtility.SetDirty(interactor);
+                Debug.Log($"Interaction layer '{interactionLayerName}' has been updated to layer {interactionLayer.value} on interactor '{interactor.gameObject.name}'.");
+            }
+        }
+
         private void SetupRaycastLayer(XRRayInteractor interactor)
         {
+            if (string.IsNullOrEmpty(raycastLayerName))
+            {
+                return;
+            }
+
             LayerMask raycastLayer = LayerMask.NameToLayer(raycastLayerName);
 
             if (raycastLayer.value < 0)
@@ -51,7 +80,7 @@ namespace VRBuilder.XRInteraction.User
             {
                 interactor.raycastMask = raycastLayer;
                 EditorUtility.SetDirty(interactor);
-                Debug.Log($"Raycast layer '{raycastLayerName}' has been updated to layer '{raycastLayer.value}' on interactor '{interactor.gameObject.name}'.");
+                Debug.Log($"Raycast layer '{raycastLayerName}' has been updated to layer {raycastLayer.value} on interactor '{interactor.gameObject.name}'.");
             }
         }
     }
