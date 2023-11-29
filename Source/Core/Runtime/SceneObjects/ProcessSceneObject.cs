cd7adb8c36fe98ea55f0589e1877595e931fd90b
VaLiuM09 <marcello.tridenti@tutanota.com>
2023-11-29 16:56:57 +0100

    ISceneObject now inherits from ITagContainer

--- Source/Core/Runtime/SceneObjects/ProcessSceneObject.cs
+++ Source/Core/Runtime/SceneObjects/ProcessSceneObject.cs
@@ -16,7 +16,7 @@ namespace VRBuilder.Core.SceneObjects
 {
     /// <inheritdoc cref="ISceneObject"/>
     [ExecuteInEditMode]
-    public class ProcessSceneObject : MonoBehaviour, ISceneObject, ITagContainer
+    public class ProcessSceneObject : MonoBehaviour, ISceneObject
     {
         public event EventHandler<LockStateChangedEventArgs> Locked;
         public event EventHandler<LockStateChangedEventArgs> Unlocked;
