562161a30cedc1ffba82fb2dcb3b73eeff7d6a53
VaLiuM09 <marcello.tridenti@tutanota.com>
2024-01-17 15:32:32 +0100

    Multiple references

--- Source/Core/Runtime/SceneObjects/MultipleSceneObjectReference.cs
+++ Source/Core/Runtime/SceneObjects/MultipleSceneObjectReference.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
+using VRBuilder.Core.Configuration;
 
 namespace VRBuilder.Core.SceneObjects
 {
@@ -17,7 +18,21 @@ namespace VRBuilder.Core.SceneObjects
 
         protected override IEnumerable<ISceneObject> DetermineValue(IEnumerable<ISceneObject> cachedValue)
         {
-            throw new NotImplementedException();
+            if (Guid == null || Guid == Guid.Empty)
+            {
+                return null;
+            }
+
+            IEnumerable<ISceneObject> value = cachedValue;
+
+            // If value exists, return it.
+            if (value != null)
+            {
+                return value;
+            }
+
+            value = RuntimeConfigurator.Configuration.SceneObjectRegistry.GetByTag(Guid);
+            return value;
         }
     }
 }
--- Source/Core/Runtime/SceneObjects/MultipleScenePropertyReference.cs
+++ Source/Core/Runtime/SceneObjects/MultipleScenePropertyReference.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Serialization;
+using VRBuilder.Core.Configuration;
+using VRBuilder.Core.Exceptions;
 using VRBuilder.Core.Properties;
 
 namespace VRBuilder.Core.SceneObjects
@@ -18,7 +21,39 @@ namespace VRBuilder.Core.SceneObjects
 
         protected override IEnumerable<T> DetermineValue(IEnumerable<T> cachedValue)
         {
-            throw new NotImplementedException();
+            if (Guid == null || Guid == Guid.Empty)
+            {
+                return null;
+            }
+
+            IEnumerable<T> value = cachedValue;
+
+            // If value exists, return it.
+            if (value != null)
+            {
+                return value;
+            }
+
+            IEnumerable<ISceneObject> sceneObject = RuntimeConfigurator.Configuration.SceneObjectRegistry.GetByTag(Guid);
+
+            // Can't find process object with given UniqueName, value is null.
+            if (sceneObject.Count() == 0)
+            {
+                return value;
+            }
+
+            value = null;
+
+            // Allows non-unique referencing system to have guid but no property
+            try
+            {
+                value = sceneObject.Select(so => so.GetProperty<T>());
+            }
+            catch (PropertyNotFoundException)
+            {
+            }
+
+            return value;
         }
     }
 }
