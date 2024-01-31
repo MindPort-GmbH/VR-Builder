544e3b8952d03a54fd7789336936f8b7107a7b7d
Markus Wellmann <mrwellmann@gmail.com>
2024-01-31 16:51:30 +0100

    Fixed Constructor and Added docs for  SceneObjectTagBase

--- Source/Core/Runtime/SceneObjects/SceneObjectTagBase.cs
+++ Source/Core/Runtime/SceneObjects/SceneObjectTagBase.cs
@@ -29,7 +29,7 @@ namespace VRBuilder.Core.SceneObjects
         }
 
         /// <summary>
-        /// List of guids, each Guid is a reference to a <seealso cref="SceneObjectTags.Tag"/>.
+        /// List of guids, each Guid is a reference to a <see cref="Settings.SceneObjectTags.Tag"/>.
         /// </summary>
         [DataMember]
         public virtual List<Guid> Guids { get; set; }
@@ -47,15 +47,21 @@ namespace VRBuilder.Core.SceneObjects
 
         internal abstract bool AllowMultipleValues { get; }
 
+        /// <summary>
+        /// Base class for behaviors and conditions to use <see cref="Settings.SceneObjectTags.Tag"/> for referencing <see cref="ProcessSceneObject"/>.
+        /// </summary>
         public SceneObjectTagBase()
         {
             Guids = new List<Guid>();
         }
 
+        /// <summary>
+        /// Base class for behaviors and conditions to use <see cref="Settings.SceneObjectTags.Tag"/> for referencing <see cref="ProcessSceneObject"/>.
+        /// </summary>
+        /// <param name="guid">Guid representing a <see cref="Settings.SceneObjectTags.Tag"/>. This usually will be <seealso cref="Guid.Empty"/> except when running automated Tests</param>
         public SceneObjectTagBase(Guid guid)
         {
-            // TODO we should not call SceneObjectTagBase with Guid.Empty
-            if (guid == null || guid == Guid.Empty)
+            if (guid == Guid.Empty)
             {
                 Guids = new List<Guid>();
             }
@@ -65,10 +71,27 @@ namespace VRBuilder.Core.SceneObjects
             }
         }
 
+
+        /// <summary>
+        /// Base class for behaviors and conditions to use <see cref="Settings.SceneObjectTags.Tag"/> for referencing <see cref="ProcessSceneObject"/>.
+        /// </summary>
+        /// <param name="guids">Representing a list of <see cref="Settings.SceneObjectTags.Tag"/>. This main reason for existence is for running automated Tests</param>
+        public SceneObjectTagBase(List<Guid> guids)
+        {
+            if (guids == null)
+            {
+                Guids = new List<Guid>();
+            }
+            else
+            {
+                Guids = guids;
+            }
+        }
+
         /// <inheritdoc />
         public virtual bool IsEmpty()
         {
-            return Guid == null || Guid == Guid.Empty;
+            return Guids == null || Guids.Count == 0 || Guids[0] == Guid.Empty;
         }
     }
 }
