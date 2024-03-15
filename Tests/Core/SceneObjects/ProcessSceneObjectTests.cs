7e50f036c9a4a89a73ab540ab1732e840c879611
Marcello Tridenti <marcello.tridenti@tutanota.com>
2024-03-15 16:28:23 +0100

    More tests

--- Tests/Core/SceneObjects/ProcessSceneObjectTests.cs
+++ Tests/Core/SceneObjects/ProcessSceneObjectTests.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
 using System;
 using System.Collections;
+using System.IO;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -43,15 +45,30 @@ namespace VRBuilder.Tests
             }
         }
 
+        private SerializableGuid GetSerializedGuid(ProcessSceneObject processSceneObject)
+        {
+            Type processSceneObjectType = typeof(ProcessSceneObject);
+            FieldInfo serializableGuidField = processSceneObjectType.GetField("serializedGuid", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (serializableGuidField != null)
+            {
+                return (SerializableGuid)serializableGuidField.GetValue(processSceneObject);
+            }
+            else
+            {
+                throw new MissingFieldException("Field 'serializedGuid' not found in class 'ProcessSceneObject'.");
+            }
+        }
+
         private void CreateFolderRecursively(string path)
         {
-            string parentPath = System.IO.Path.GetDirectoryName(path);
+            string parentPath = Path.GetDirectoryName(path);
             if (!AssetDatabase.IsValidFolder(parentPath))
             {
                 CreateFolderRecursively(parentPath);
             }
 
-            AssetDatabase.CreateFolder(parentPath, System.IO.Path.GetFileName(path));
+            AssetDatabase.CreateFolder(parentPath, Path.GetFileName(path));
         }
 
         private GameObject CreatePrefab(GameObject originalObject)
@@ -85,15 +102,14 @@ namespace VRBuilder.Tests
             // Given a PSO,
             string prefabName = GetUniquePrefabName();
             ProcessSceneObject processSceneObject = new GameObject(prefabName).AddComponent<ProcessSceneObject>();
-
-            Guid uniqueId = processSceneObject.Guid;
+            Guid uniqueId = GetSerializedGuid(processSceneObject).Guid;
 
             // When I save it as a prefab,
-            GameObject loadedPrefab = CreatePrefab(processSceneObject.gameObject);
+            GameObject prefab = CreatePrefab(processSceneObject.gameObject);
 
             // Then it does not retain its unique id.
             Assert.AreNotEqual(Guid.Empty, uniqueId);
-            Assert.AreEqual(Guid.Empty, loadedPrefab.GetComponent<ProcessSceneObject>().Guid);
+            Assert.IsNull(GetSerializedGuid(prefab.GetComponent<ProcessSceneObject>()));
 
             // Cleanup
             DeletePrefab(prefabName);
@@ -112,9 +128,12 @@ namespace VRBuilder.Tests
             ProcessSceneObject spawnedObject2 = ((GameObject)PrefabUtility.InstantiatePrefab(processSceneObjectPrefab)).GetComponent<ProcessSceneObject>();
 
             // Then each spawned object has a different unique id.
-            Assert.AreNotEqual(Guid.Empty, spawnedObject1.Guid);
-            Assert.AreNotEqual(Guid.Empty, spawnedObject2.Guid);
-            Assert.AreNotEqual(spawnedObject1.Guid, spawnedObject2.Guid);
+            Guid guid1 = GetSerializedGuid(spawnedObject1).Guid;
+            Guid guid2 = GetSerializedGuid(spawnedObject2).Guid;
+
+            Assert.AreNotEqual(Guid.Empty, guid1);
+            Assert.AreNotEqual(Guid.Empty, guid2);
+            Assert.AreNotEqual(guid1, guid2);
 
             // Cleanup
             DeletePrefab(prefabName);
@@ -130,22 +149,93 @@ namespace VRBuilder.Tests
             string prefabName = GetUniquePrefabName();
             GameObject prefab = CreateProcessSceneObjectPrefab(prefabName);
             GameObject instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
-            AssetDatabase.Refresh();
-            Guid uniqueId = instance.GetComponent<ProcessSceneObject>().Guid;
 
-            // If I modify it and then apply the changes,
+            // When I modify it and then apply the changes,
             instance.AddComponent<Rigidbody>();
             PrefabUtility.ApplyPrefabInstance(instance, InteractionMode.AutomatedAction);
 
             // Then the unique id on the original prefab is not overwritten.
             Assert.IsNotNull(prefab.GetComponent<Rigidbody>());
-            Assert.AreNotEqual(uniqueId, prefab.GetComponent<ProcessSceneObject>().Guid);
-            Assert.AreEqual(Guid.Empty, prefab.GetComponent<ProcessSceneObject>().Guid);
+            Assert.IsTrue(GetSerializedGuid(instance.GetComponent<ProcessSceneObject>()).IsValid());
+            Assert.IsFalse(GetSerializedGuid(prefab.GetComponent<ProcessSceneObject>()).IsValid());
 
             // Cleanup
             yield return null;
             DeletePrefab(prefabName);
             GameObject.DestroyImmediate(instance);
+        }
+
+        [UnityTest]
+        public IEnumerator ResettingInstanceChangesDoesNotChangeGuid()
+        {
+            // Given an instance of a PSO prefab,
+            string prefabName = GetUniquePrefabName();
+            GameObject prefab = CreateProcessSceneObjectPrefab(prefabName);
+            GameObject instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            Guid instanceGuid = GetSerializedGuid(instance.GetComponent<ProcessSceneObject>()).Guid;
+
+            // When I modify it and then reset the changes,
+            instance.AddComponent<Rigidbody>();
+            PrefabUtility.RevertPrefabInstance(instance, InteractionMode.AutomatedAction);
+
+            // The unique id of the instance stays the same.
+            Assert.IsNull(instance.GetComponent<Rigidbody>());
+            Assert.AreEqual(instanceGuid, GetSerializedGuid(instance.GetComponent<ProcessSceneObject>()).Guid);
+
+            // Cleanup
+            yield return null;
+            DeletePrefab(prefabName);
+            GameObject.DestroyImmediate(instance);
+        }
+
+        [UnityTest]
+        public IEnumerator WhenAddingToPrefabProcessSceneObjectInstancesObtainDifferentGuids()
+        {
+            // Given a prefab of which multiple instances are present in the scene,
+            string prefabName = GetUniquePrefabName();
+            GameObject prefab = CreatePrefab(new GameObject(prefabName));
+            GameObject instance1 = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            GameObject instance2 = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+
+            // When I add a process scene object to it,
+            prefab.AddComponent<ProcessSceneObject>();
+            PrefabUtility.SavePrefabAsset(prefab);
+
+            // Then all instances of the prefab have a different unique id.
+            SerializableGuid guid1 = GetSerializedGuid(instance1.GetComponent<ProcessSceneObject>());
+            SerializableGuid guid2 = GetSerializedGuid(instance2.GetComponent<ProcessSceneObject>());
+
+            Assert.IsTrue(guid1.IsValid());
+            Assert.IsTrue(guid2.IsValid());
+            Assert.AreNotEqual(guid1, guid2);
+
+            // Cleanup
+            yield return null;
+            DeletePrefab(prefabName);
+            GameObject.DestroyImmediate(instance1);
+            GameObject.DestroyImmediate(instance2);
+        }
+
+        [UnityTest]
+        public IEnumerator DuplicatedObjectsHaveDifferentUniqueIds()
+        {
+            // Given a PSO in the scene,
+            ProcessSceneObject original = GameObject.Instantiate(new GameObject("Test")).AddComponent<ProcessSceneObject>();
+            Guid originalGuid = GetSerializedGuid(original).Guid;
+
+            // When I duplicate it by re-instantiating it,
+            ProcessSceneObject copy = GameObject.Instantiate(original) as ProcessSceneObject;
+            Guid copyGuid = GetSerializedGuid(copy).Guid;
+
+            // Then the new object has a different unique id.
+            Assert.AreNotEqual(Guid.Empty, originalGuid);
+            Assert.AreNotEqual(Guid.Empty, copyGuid);
+            Assert.AreNotEqual(originalGuid, copyGuid);
+
+            // Cleanup
+            GameObject.DestroyImmediate(original);
+            GameObject.DestroyImmediate(copy);
+            yield return null;
         }
     }
 }
