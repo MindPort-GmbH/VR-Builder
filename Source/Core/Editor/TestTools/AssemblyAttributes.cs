0f181d4b365c791789671dcaebec29afa22b25ab
Markus Wellmann <mrwellmann@gmail.com>
2024-03-19 12:43:19 +0100

    Removed Obsolete EditorImguiTester TestTools

--- Source/Core/Editor/TestTools/AssemblyAttributes.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2013-2019 Innoactive GmbH
-// Licensed under the Apache License, Version 2.0
-// Modifications copyright (c) 2021-2024 MindPort GmbH
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("VRBuilder.Core.Tests.Editmode")]
