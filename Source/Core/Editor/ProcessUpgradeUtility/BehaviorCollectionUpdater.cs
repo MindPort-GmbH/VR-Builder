5c0aa96df2884782e2639eb097b06c8120a3bb05
Marcello Tridenti <marcello.tridenti@tutanota.com>
2024-03-28 17:15:28 +0100

    Working hack

--- Source/Core/Editor/ProcessUpgradeUtility/BehaviorCollectionUpdater.cs
+++ Source/Core/Editor/ProcessUpgradeUtility/BehaviorCollectionUpdater.cs
@@ -1,32 +1,37 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
 using VRBuilder.Core.Behaviors;
+using VRBuilder.Core.Utils;
 
 namespace VRBuilder.Editor.Utils
 {
     public class BehaviorCollectionUpdater : IUpdater
     {
-        public Type SupportedType => typeof(IEnumerable<IBehavior>);
+        public Type SupportedType => typeof(IList<IBehavior>);
 
         public void Update(MemberInfo memberInfo, object owner)
         {
-            IEnumerable<IBehavior> updatedObject = ReflectionUtils.GetValueFromPropertyOrField(owner, memberInfo) as IEnumerable<IBehavior>;
+            IList<IBehavior> behaviorList = ReflectionUtils.GetValueFromPropertyOrField(owner, memberInfo) as IList<IBehavior>;
 
             Debug.Log("Updating behavior list");
 
-            foreach (IBehavior behavior )
-
-                foreach (MemberInfo propertyMemberInfo in properties)
+            for (int i = 0; i < behaviorList.Count; i++)
+            {
+                if (behaviorList[i] is SetObjectsWithTagEnabledBehavior)
                 {
-                    Type type = ReflectionUtils.GetDeclaredTypeOfPropertyOrField(propertyMemberInfo);
-                    IUpdater updater = ProcessUpdater.GetUpdaterForType(type);
+#pragma warning disable CS0618 // Type or member is obsolete
+                    SetObjectsWithTagEnabledBehavior oldBehavior = behaviorList[i] as SetObjectsWithTagEnabledBehavior;
+#pragma warning restore CS0618 // Type or member is obsolete
+                    SetObjectsEnabledBehavior newBehavior = new SetObjectsEnabledBehavior();
+                    newBehavior.Data.SetEnabled = oldBehavior.Data.SetEnabled;
+                    newBehavior.Data.TargetObjects = oldBehavior.Data.TargetObjects;
+                    newBehavior.Data.RevertOnDeactivation = oldBehavior.Data.RevertOnDeactivation;
 
-                    if (updater != null)
-                    {
-                        updater.Update(propertyMemberInfo, updatedObject);
-                    }
+                    behaviorList[i] = newBehavior;
                 }
-
+            }
         }
     }
 }
