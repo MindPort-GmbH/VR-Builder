aa54bb69ff135b0fe1da5478b812cdf433ec54f9
Markus Wellmann <mrwellmann@gmail.com>
2024-04-17 13:56:09 +0200

    Moved SceneReferencesEditorPopup to Source/Core/Editor/UI/GraphView/

--- Source/Core/Editor/UI/GraphView/SceneReferencesEditorPopup.cs
+++ Source/Core/Editor/UI/GraphView/SceneReferencesEditorPopup.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 using VRBuilder.Core.Configuration;
