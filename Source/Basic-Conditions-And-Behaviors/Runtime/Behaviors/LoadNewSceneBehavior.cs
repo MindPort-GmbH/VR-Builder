2d13d53de9b56bf2f5d394336a501bed1f1f5342
Marcello Tridenti <marcello.tridenti@tutanota.com>
2024-06-27 10:35:10 +0200

    Created files

---
+++ Source/Basic-Conditions-And-Behaviors/Runtime/Behaviors/LoadNewSceneBehavior.cs
@@ -0,0 +1,95 @@
+using Newtonsoft.Json;
+using System.Collections;
+using System.Runtime.Serialization;
+using UnityEngine;
+using UnityEngine.Scripting;
+using VRBuilder.Core.Attributes;
+
+namespace VRBuilder.Core.Behaviors
+{
+    /// <summary>
+    /// Behavior that waits for `DelayTime` seconds before finishing its activation.
+    /// </summary>
+    [DataContract(IsReference = true)]
+    [HelpLink("https://www.mindport.co/vr-builder/manual/default-behaviors/delay")]
+    public class LoadNewSceneBehavior : Behavior<LoadNewSceneBehavior.EntityData>
+    {
+        /// <summary>
+        /// The data class for a delay behavior.
+        /// </summary>
+        [DisplayName("Delay")]
+        [DataContract(IsReference = true)]
+        public class EntityData : IBehaviorData
+        {
+            [DataMember]
+            [DisplayName("Delay (in seconds)")]
+            public float DelayTime { get; set; }
+
+            public Metadata Metadata { get; set; }
+
+            [IgnoreDataMember]
+            public string Name
+            {
+                get
+                {
+                    return $"Wait for {DelayTime} seconds";
+                }
+            }
+        }
+
+        [JsonConstructor, Preserve]
+        public LoadNewSceneBehavior() : this(0)
+        {
+        }
+
+        public LoadNewSceneBehavior(float delayTime)
+        {
+            if (delayTime < 0f)
+            {
+                Debug.LogWarningFormat("DelayTime has to be zero or positive, but it was {0}. Setting to 0 instead.", delayTime);
+                delayTime = 0f;
+            }
+
+            Data.DelayTime = delayTime;
+        }
+
+        private class ActivatingProcess : StageProcess<EntityData>
+        {
+            public ActivatingProcess(EntityData data) : base(data)
+            {
+            }
+
+            /// <inheritdoc />
+            public override void Start()
+            {
+            }
+
+            /// <inheritdoc />
+            public override IEnumerator Update()
+            {
+                float timeStarted = Time.time;
+
+                while (Time.time - timeStarted < Data.DelayTime)
+                {
+                    yield return null;
+                }
+            }
+
+            /// <inheritdoc />
+            public override void End()
+            {
+            }
+
+            /// <inheritdoc />
+            public override void FastForward()
+            {
+            }
+        }
+
+        /// <inheritdoc />
+        public override IStageProcess GetActivatingProcess()
+        {
+            return new ActivatingProcess(Data);
+        }
+    }
+}
