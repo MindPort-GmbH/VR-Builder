feb24aff712c908575975fb71a33bf67ae0f82cd
Marcello Tridenti <marcello.tridenti@tutanota.com>
2024-07-23 10:20:43 +0200

    Added resource drawer for animation clips

---
+++ Source/Core/Editor/UI/Drawers/AnimationClipResourceDrawer.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace VRBuilder.Editor.UI.Drawers
+{
+    /// <summary>
+    /// Implementation of asset path drawer for <see cref="AnimationClip"/> assets.
+    /// </summary>
+    public class AnimationClipResourceDrawer : ResourcePathDrawer<AnimationClip>
+    {
+    }
+}
