a1eac06e98d8e09cce16f2e37dba21a32d6e1d6a
Marcello Tridenti <marcello.tridenti@tutanota.com>
2024-08-30 17:03:21 +0200

    Aborting is not instant

--- Source/Core/Runtime/EntityOwners/Parallel/ParallelAbortingProcess.cs
+++ Source/Core/Runtime/EntityOwners/Parallel/ParallelAbortingProcess.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Linq;
 using UnityEngine;
 using VRBuilder.Core.Configuration.Modes;
 
@@ -6,19 +8,35 @@ namespace VRBuilder.Core.EntityOwners.ParallelEntityCollection
     /// <summary>
     /// A process over a collection of entities which aborts them at the same time, in parallel.
     /// </summary>
-    internal class ParallelAbortingProcess<TCollectionData> : InstantProcess<TCollectionData> where TCollectionData : class, IEntityCollectionData, IModeData
+    internal class ParallelAbortingProcess<TCollectionData> : StageProcess<TCollectionData> where TCollectionData : class, IEntityCollectionData, IModeData
     {
         public ParallelAbortingProcess(TCollectionData data) : base(data)
+        {
+        }
+
+        public override void End()
+        {
+        }
+
+        public override void FastForward()
         {
         }
 
         /// <inheritdoc />
         public override void Start()
         {
-            foreach (IEntity child in Data.GetChildren())
+            foreach (IEntity child in Data.GetChildren().Where(child => child.LifeCycle.Stage != Stage.Inactive))
             {
                 Debug.Log($"Aborting ´{child}");
                 child.LifeCycle.Abort();
+            }
+        }
+
+        public override IEnumerator Update()
+        {
+            while (Data.GetChildren().Any(child => child.LifeCycle.Stage != Stage.Inactive))
+            {
+                yield return null;
             }
         }
     }
