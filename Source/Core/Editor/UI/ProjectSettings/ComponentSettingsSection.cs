4f4ab54a30c22bd2490a55d75b6d21315178c68b
Marcello Tridenti <marcello.tridenti@tutanota.com>
2024-09-13 15:58:14 +0200

    Generic component settings section

--- Source/Core/Editor/UI/ProjectSettings/ComponentSettingsSection.cs
+++ Source/Core/Editor/UI/ProjectSettings/ComponentSettingsSection.cs
@@ -1,18 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Reflection;
+using UnityEditor;
 using UnityEngine;
+using VRBuilder.Core.Runtime.Utils;
 
-public class ComponentSettingsSection : MonoBehaviour
+namespace VRBuilder.Editor.UI
 {
-    // Start is called before the first frame update
-    void Start()
+    public abstract class ComponentSettingsSection<TObject, TSettings> : IProjectSettingsSection where TObject : UnityEngine.Object where TSettings : ComponentSettings<TObject, TSettings>, new()
     {
-        
-    }
+        public abstract string Title { get; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public abstract string Description { get; }
+
+        public Type TargetPageProvider => typeof(ComponentSettingsProvider);
+
+        public abstract int Priority { get; }
+
+        private UnityEditor.Editor editor;
+        private TSettings settings;
+
+        public void OnGUI(string searchContext)
+        {
+            EditorGUILayout.Space();
+            GUIStyle labelStyle = BuilderEditorStyles.ApplyPadding(BuilderEditorStyles.Paragraph, 0);
+            GUILayout.Label(Description, labelStyle);
+            EditorGUILayout.Space();
+
+            editor.OnInspectorGUI();
+
+            EditorGUILayout.Space(20f);
+
+            if (GUILayout.Button("Apply settings in current scene"))
+            {
+                TObject[] objects = Resources.FindObjectsOfTypeAll<TObject>();
+
+                foreach (TObject snapZone in objects)
+                {
+                    GetSettingsInstance().ApplySettings(snapZone);
+                }
+            }
+        }
+
+        public ComponentSettingsSection()
+        {
+            editor = UnityEditor.Editor.CreateEditor(GetSettingsInstance());
+        }
+
+        private TSettings GetSettingsInstance()
+        {
+            if (settings != null)
+            {
+                return settings;
+            }
+
+            Type closedGenericType = typeof(SettingsObject<>).MakeGenericType(typeof(TSettings));
+
+            PropertyInfo instanceProperty = closedGenericType.GetProperty("Instance", BindingFlags.Static | BindingFlags.Public);
+
+            if (instanceProperty != null)
+            {
+                return instanceProperty.GetValue(null) as TSettings;
+            }
+
+            return null;
+        }
     }
 }
--- Source/XR-Interaction-Component/Source/Editor/ProjectSettings/SnapZoneSettingsSection.cs
+++ Source/XR-Interaction-Component/Source/Editor/ProjectSettings/SnapZoneSettingsSection.cs
@@ -1,46 +1,14 @@
-using System;
-using UnityEditor;
-using UnityEngine;
 using VRBuilder.Editor.XRInteraction;
 using VRBuilder.XRInteraction;
 
 namespace VRBuilder.Editor.UI
 {
-    public class SnapZoneSettingsSection : IProjectSettingsSection
+    public class SnapZoneSettingsSection : ComponentSettingsSection<SnapZone, SnapZoneSettings>
     {
-        public string Title => "Snap Zones";
+        public override string Title => "Snap Zones";
 
-        public Type TargetPageProvider => typeof(ComponentSettingsProvider);
+        public override string Description => "These settings help you to configure Snap Zones within your scenes. You can define colors and other values that will be set to Snap Zones created by clicking the 'Create Snap Zone' button of a Snappable Property.";
 
-        public int Priority => 1000;
-
-        private UnityEditor.Editor editor;
-
-        public void OnGUI(string searchContext)
-        {
-            EditorGUILayout.Space();
-            GUIStyle labelStyle = BuilderEditorStyles.ApplyPadding(BuilderEditorStyles.Paragraph, 0);
-            GUILayout.Label("These settings help you to configure Snap Zones within your scenes. You can define colors and other values that will be set to Snap Zones created by clicking the 'Create Snap Zone' button of a Snappable Property.", labelStyle);
-            EditorGUILayout.Space();
-
-            editor.OnInspectorGUI();
-
-            EditorGUILayout.Space(20f);
-
-            if (GUILayout.Button("Apply settings in current scene"))
-            {
-                SnapZone[] snapZones = Resources.FindObjectsOfTypeAll<SnapZone>();
-
-                foreach (SnapZone snapZone in snapZones)
-                {
-                    SnapZoneSettings.Instance.ApplySettings(snapZone);
-                }
-            }
-        }
-
-        public SnapZoneSettingsSection()
-        {
-            editor = UnityEditor.Editor.CreateEditor(SnapZoneSettings.Instance);
-        }
+        public override int Priority => 1000;
     }
 }
