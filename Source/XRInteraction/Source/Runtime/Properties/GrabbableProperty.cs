bc1eac9a9bd8fc9024f119019cdcda8b504e77e6
Markus Wellmann <mrwellmann@gmail.com>
2024-12-19 14:13:08 +0100

    Added RequireComponent InteractableObject

--- Source/XRInteraction/Source/Runtime/Properties/GrabbableProperty.cs
+++ Source/XRInteraction/Source/Runtime/Properties/GrabbableProperty.cs
@@ -13,6 +13,7 @@ namespace VRBuilder.XRInteraction.Properties
     /// <summary>
     /// XR implementation of <see cref="IGrabbableProperty"/>.
     /// </summary>
+    [RequireComponent(typeof(InteractableObject))]
     public class GrabbableProperty : LockableProperty, IGrabbableProperty
     {
         [Header("Events")]
