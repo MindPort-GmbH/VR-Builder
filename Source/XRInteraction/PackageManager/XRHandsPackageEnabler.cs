44a33cbdf3d92dbca0620c1aaaa6111c572c14f2
Markus Wellmann <mrwellmann@gmail.com>
2025-08-14 14:52:41 +0200

    Added dependency on com.unity.xr.hands

---
+++ Source/XRInteraction/PackageManager/XRHandsPackageEnabler.cs
@@ -0,0 +1,17 @@
+﻿namespace VRBuilder.PackageManager.Editor.XRInteraction
+{
+    /// <summary>
+    /// Adds Unity's XR-Interaction-Toolkit package as a dependency and sets specified symbol for script compilation.
+    /// </summary>
+    public class XRHandsPackageEnabler : Dependency
+    {
+        /// <inheritdoc/>
+        public override string Package { get; } = "com.unity.xr.hands";
+
+        /// <inheritdoc/>
+        public override string Version { get; set; } = "1.5.1";
+
+        /// <inheritdoc/>
+        public override int Priority { get; } = 4;
+    }
+}
