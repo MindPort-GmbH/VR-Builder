e185f6f3a75be59eec9aa3fe204b68d66310e6ec
Markus Wellmann <mrwellmann@gmail.com>
2025-09-05 13:40:06 +0200

    Hidden StreamingAssets

--- Demo/Editor/DemoSceneLoader.cs
+++ Demo/Editor/DemoSceneLoader.cs
@@ -14,8 +14,8 @@ namespace VRBuilder.Demo.Editor
         private const string demoSceneAssetsPath = "Assets/MindPort/VR Builder/Core/Demo/Scenes/VR Builder Demo - Core Features.unity";
         private const string demoScenePackagesPath = "Packages/co.mindport.vrbuilder.core/Demo/Scenes/VR Builder Demo - Core Features.unity";
 
-        private const string demoProcessAssetsPath = "Assets/MindPort/VR Builder/Core/Demo/StreamingAssets/Processes/Demo - Core Features/Demo - Core Features.json";
-        private const string demoProcessPackagesPath = "Packages/co.mindport.vrbuilder.core/Demo/StreamingAssets/Processes/Demo - Core Features/Demo - Core Features.json";
+        private const string demoProcessAssetsPath = "Assets/MindPort/VR Builder/Core/Demo/StreamingAssets~/Processes/Demo - Core Features/Demo - Core Features.json";
+        private const string demoProcessPackagesPath = "Packages/co.mindport.vrbuilder.core/Demo/StreamingAssets~/Processes/Demo - Core Features/Demo - Core Features.json";
 
         private const string demoProcessTargetPath = "Assets/StreamingAssets/Processes/Demo - Core Features/Demo - Core Features.json";
         private const string demoProcessTargetDirectory = "Assets/StreamingAssets/Processes/Demo - Core Features";
