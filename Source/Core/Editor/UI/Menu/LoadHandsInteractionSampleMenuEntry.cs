c3ed74edc2ccccbb6b46be67fb5c8cc0222b59a3
Markus Wellmann <mrwellmann@gmail.com>
2025-09-11 15:22:41 +0200

    Added import pop up for Demo - Hands Interaction and updated package.json accordingly

--- Source/Core/Editor/UI/Menu/LoadHandsInteractionSampleMenuEntry.cs
+++ Source/Core/Editor/UI/Menu/LoadHandsInteractionSampleMenuEntry.cs
@@ -16,6 +16,26 @@ namespace VRBuilder.Core.Editor
         {
             bool isPackage = !Directory.Exists(assetsSampleSourcePath);
 
+            int result = EditorUtility.DisplayDialogComplex(
+                "Demo - Hands Interaction",
+                "This demo includes a sample scene for hand-tracking integration with VR Builder and the XR Interaction Toolkit.\n\n" +
+                "If not already present, importing this demo will install the following dependencies:\n" +
+                " • Package: XR Hands (com.unity.xr.hands).\n" +
+                " • Package: Shader Graph (com.unity.shadergraph).\n" +
+                " • Sample: XR Hands - HandVisualizer.\n" +
+                " • Sample: XRI - Starter Assets.\n" +
+                " • Sample: XRI - Hands Interaction Demo.\n\n" +
+                "It may take a bit of time, but at the end the sample scene will be opened automatically.",
+                "Continue",
+                "Cancel",
+                ""
+            );
+
+            if (result != 0)
+            {
+                return;
+            }
+
             if (isPackage)
             {
                 SampleImporter.ImportSampleFromPackage(packageName, sampleName);
