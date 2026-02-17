2f628c8c9556b0201ec98b6def62af39e69befce
Marcello Tridenti <marcello.tridenti@tutanota.com>
2026-02-17 10:50:13 +0100

    Removed unnecessary using

--- Source/PackageManager/PackageManager/PackageOperationsManager.cs
+++ Source/PackageManager/PackageManager/PackageOperationsManager.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
