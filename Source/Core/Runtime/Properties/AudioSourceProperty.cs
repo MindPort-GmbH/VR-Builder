16aba71d1cdda2204ef811d362b3a9cc6e4204cd
poetter-sebastian <32681913+poetter-sebastian@users.noreply.github.com>
2026-02-26 11:41:41 +0100

    Added bridge for using custom audio sources

---
+++ Source/Core/Runtime/Properties/AudioSourceProperty.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace VRBuilder.Core.Properties
+{
+    /// <summary>
+    /// Implementation of the <see cref="IAudioSourceProperty"/> class.
+    /// </summary>
+    public class AudioSourceProperty: ProcessSceneObjectProperty, IAudioSourceProperty
+    {
+        /// <inheritdoc/>
+        public AudioSource AudioPlayer
+        {
+            get
+            {
+                return audioSource ??= GetComponent<AudioSource>();
+            }
+        }
+        
+        [Header("Settings")]
+        [SerializeField]
+        private AudioSource audioSource;
+    }
+}
---
+++ Source/Core/Runtime/Properties/IAudioSourceProperty.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace VRBuilder.Core.Properties
+{
+    /// <summary>
+    /// Property for the connection of a scene-based audio player.
+    /// </summary>
+    public interface IAudioSourceProperty : ISceneObjectProperty
+    {
+        /// <summary>
+        /// Get the selected audio player.
+        /// </summary>
+        public AudioSource AudioPlayer { get; }
+    }
+}
