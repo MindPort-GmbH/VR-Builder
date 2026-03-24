eea537623cbf90df5300444c9f5af5efe995dbac
Zaid Wajid <xaidwajid@gmail.com>
2026-03-24 15:48:02 +0100

    fix: old aliases bug fix
    fixed a bug after refactoring appeared due to some name changes
    
    Ref: VRB-2933

--- Source/Core/Runtime/Behaviors/HighlightObjectBehavior.cs
+++ Source/Core/Runtime/Behaviors/HighlightObjectBehavior.cs
@@ -1,10 +1,13 @@
+using Newtonsoft.Json;
 using System;
 using System.Runtime.Serialization;
 using UnityEngine;
+using UnityEngine.Scripting;
 using VRBuilder.Core.Attributes;
 using VRBuilder.Core.Configuration.Modes;
 using VRBuilder.Core.Properties;
 using VRBuilder.Core.SceneObjects;
+using VRBuilder.Core.Utils;
 
 namespace VRBuilder.Core.Behaviors
 {
@@ -146,6 +149,8 @@ namespace VRBuilder.Core.Behaviors
     [HelpLink("https://www.mindport.co/vr-builder/manual/default-behaviors/highlight-object")]
     public class HighlightObjectBehavior : AbstractColorHighlightBehavior<HighlightObjectBehavior.EntityData, IHighlightProperty>, IObjectHighlightBehavior
     {
+        private static readonly Color32 defaultHighlightColor = new Color32(231, 64, 255, 126);
+
         /// <summary>
         /// "Highlight object" behavior's data.
         /// </summary>
@@ -153,16 +158,23 @@ namespace VRBuilder.Core.Behaviors
         [DataContract(IsReference = true)]
         public class EntityData : IBehaviorData, IColorHighlightBehaviorData<IHighlightProperty>
         {
+            private ModeParameter<Color> customColor;
+
             /// <summary>
             /// <see cref="ModeParameter{T}"/> of the highlight color.
             /// Process modes can change the highlight color.
             /// </summary>
-            public ModeParameter<Color> CustomColor { get; set; }
+            public ModeParameter<Color> CustomColor
+            {
+                get { return customColor ??= new ModeParameter<Color>("HighlightColor", defaultHighlightColor); }
+                set { customColor = value; }
+            }
 
             /// <summary>
             /// Highlight color set in the Step Inspector.
             /// </summary>
             [DataMember(Name = "HighlightColor")]
+            [JsonProperty("HighlightColor")]
             [DisplayName("Color")]
             public Color Color
             {
@@ -185,6 +197,23 @@ namespace VRBuilder.Core.Behaviors
             [IgnoreDataMember]
             public string Name => $"Highlight {TargetObjects}";
 
+        }
+
+        [JsonConstructor, Preserve]
+        public HighlightObjectBehavior() : this(Guid.Empty, defaultHighlightColor)
+        {
+        }
+
+        public HighlightObjectBehavior(Guid objectId, Color highlightColor) : base(objectId, highlightColor)
+        {
+        }
+
+        public HighlightObjectBehavior(IHighlightProperty target) : this(target, defaultHighlightColor)
+        {
+        }
+
+        public HighlightObjectBehavior(IHighlightProperty target, Color highlightColor) : this(ProcessReferenceUtils.GetUniqueIdFrom(target), highlightColor)
+        {
         }
 
         /// <inheritdoc />
