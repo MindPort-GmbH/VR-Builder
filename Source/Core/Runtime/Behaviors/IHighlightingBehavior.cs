70ddc6c169bd2f8cc1c8d3a32f49cfc26e603f30
Marcello Tridenti <marcello.tridenti@tutanota.com>
2026-05-06 15:20:28 +0200

    Moved files to submodule

--- Source/Core/Runtime/Behaviors/IHighlightingBehavior.cs
+++
@@ -1,9 +0,0 @@
-namespace VRBuilder.Core.Behaviors
-{
-    /// <summary>
-    /// Marker interface for all behaviors that highlight scene objects.
-    /// </summary>
-    public interface IHighlightingBehavior : IBehavior
-    {
-    }
-}
