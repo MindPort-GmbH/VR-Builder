8c24b6457d8bd5303c33d7218ac3a4d6d9622eae
Zaid Wajid <48151120+ScarEye@users.noreply.github.com>
2026-06-25 16:47:40 +0200

    Feature/vrb 2141  basic log window for vr builds (#370)
    * Updated Console Prefab
    
    Updated console error window Prefab
    
    Ref: VRB-2141
    
    * add: Unity dependent console scripts
    
    Added console scripts which are dependent on Unity
    
    Ref: VRB-2141
    
    * update: console dependency
    
    Fixing some bugs and unity dependency on console
    
    Ref: VRB-2141
    
    * fix: interaction bugs
    
    Fixed some interaction bugs
    
    Ref: VRB-2141
    
    * fix: UI
    
    Fixed Height of List UI Element
    
    Ref: VRB-2141
    
    * remove: assembly
    
    Removed assembly references for console log
    
    Ref: VRB-2141
    
    * Submodule pointer
    
    * add: core assembly reference
    
    Added core assembly reference
    
    Ref: VRB-2141
    
    ---------
    
    Co-authored-by: Marcello Tridenti <marcello.tridenti@tutanota.com>

---
+++ Source/UI/Console/VRBConsolePlacer.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using VRBuilder.Core.Configuration;
+
+namespace VRBuilder.UI.Console
+{
+    /// <summary>
+    /// Places the console in front of the user. Triggered once every time the console is shown,
+    /// the console does not follow the user afterwards.
+    /// </summary>
+    public class VRBConsolePlacer : MonoBehaviour
+    {
+        [SerializeField]
+        private float distanceFromUser = 1.75f;
+
+        [SerializeField]
+        private float heightOffset = -0.4f;
+
+        /// <summary>
+        /// Positions the console in front of the user at eye height and rotates it to face them.
+        /// </summary>
+        public void PlaceInFrontOfUser()
+        {
+            Transform head = GetUserHead();
+
+            if (head == null)
+            {
+                return;
+            }
+
+            Vector3 forward = Vector3.ProjectOnPlane(head.forward, Vector3.up);
+
+            if (forward.sqrMagnitude < 0.0001f)
+            {
+                forward = Vector3.ProjectOnPlane(head.up, Vector3.up);
+            }
+
+            forward.Normalize();
+
+            Vector3 position = head.position + forward * distanceFromUser + Vector3.up * heightOffset;
+            transform.SetPositionAndRotation(position, Quaternion.LookRotation(forward, Vector3.up));
+        }
+
+        private Transform GetUserHead()
+        {
+            if (RuntimeConfigurator.Exists && RuntimeConfigurator.Configuration.User != null)
+            {
+                return RuntimeConfigurator.Configuration.User.Head;
+            }
+
+            return Camera.main != null ? Camera.main.transform : null;
+        }
+    }
+}
