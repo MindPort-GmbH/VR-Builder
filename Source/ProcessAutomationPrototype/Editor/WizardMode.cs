2914ae796804cae4873b0b5646088d9331e46e78
Markus Wellmann <mrwellmann@gmail.com>
2026-07-16 00:53:46 +0200

    refactor: extract process automation package

--- Source/ProcessAutomationPrototype/Editor/WizardMode.cs
+++
@@ -1,9 +0,0 @@
-namespace VRBuilder.ProcessAutomationPrototype.Editor
-{
-    /// <summary>Active creation mode in the Process Wizard.</summary>
-    public enum WizardMode
-    {
-        Guided = 0,
-        Prompt = 1,
-    }
-}
